1f06c04a01bc960a06d8a64422749558cb8a030f
Brian L. McMichael <brian@brianmcmichael.com>
2013-02-18 10:53:54 -0800

    Create QuickUnionUF.java

---
+++ QuickUnionUF.java
@@ -0,0 +1,35 @@
+package com.brianmcmichael;
+
+public class QuickUnionUF
+{
+  private int[] id;
+	
+	public QuickUnionUF(int N)
+	{
+		id = new int[N];
+		for (int i = 0; i < N; i++) id[i] = i;
+	}
+	
+	private int root(int i)
+	{
+		while (i != id[i]) i = id[i];
+		return i;
+	}
+	
+	public boolean connected(int p, int q)
+	{
+		return root(p) == root(q);
+	}
+	
+	public void union(int p, int q)
+	{
+		int i = root(p);
+		int j = root(q);
+		id[i] = j;
+	}
+	
+	public int[] getId()
+	{
+		return id;
+	}
+}			
