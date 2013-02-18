84f40697aa304ccb1510e629992079c69b67b667
Brian L. McMichael <brian@brianmcmichael.com>
2013-02-18 11:20:03 -0800

    Create FixedCapacityStackOfStrings.java

---
+++ FixedCapacityStackOfStrings.java
@@ -0,0 +1,20 @@
+package com.brianmcmichael;
+
+public class FixedCapacityStackOfStrings 
+{
+  private String[] s;
+	private int N = 0;
+	
+	//A cheat to limit capacity by requiring input
+	public FixedCapacityStackOfStrings(int capacity)
+	{	s = new String[capacity];	}
+	
+	public boolean isEmpty()
+	{	return N == 0;	}
+	
+	public void push(String item)
+	{	s[N++] = item; }
+	
+	public String pop()
+	{	return s[--N];	}
+}
