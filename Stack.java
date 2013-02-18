1362759175202952f1680de4a1b7d3f143a55cb5
Brian L. McMichael <brian@brianmcmichael.com>
2013-02-18 15:12:04 -0500

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,8 +1,10 @@
 package com.brianmcmichael;
 
-public class Stack<Item> 
+import java.util.Iterator;
+
+public class Stack<Item> implements Iterable<Item>
 {
-  private Node first = null;
+	private Node first = null;
 	
 	private class Node
 	{
@@ -20,4 +22,42 @@ public class Stack<Item>
 		first.item = item;
 		first.next = oldfirst;
 	}
+	
+	public Item pop()
+	{
+		Item item = first.item;
+		first = first.next;
+		return item;
+	}
+	
+	public Iterator<Item> iterator() { return new ListIterator(); }
+	
+	private class ListIterator implements Iterator<Item>
+	{
+		private Node current = first;
+		
+		public boolean hasNext() 	{ return current != null; }
+		public void remove()		{ /* Not Implemented */ }
+		public Item next()
+		{
+			Item item = current.item;
+			current	= current.next;
+			return item;
+		}
+	}
+	
+	/*  
+	public Iterator<Item> iterator()
+    { return new ReverseArrayIterator(); }
+    
+	private class ReverseArrayIterator implements Iterator<Item>
+    {
+        private int i = N;
+        public boolean hasNext() {  return i > 0;        }
+        public void remove()     {  					 }
+        public Item next()       {  return s[--i];       }
+    }
+    */
+	
+	
 }
